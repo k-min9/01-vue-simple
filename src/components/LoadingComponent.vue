510469a556dc5d0f1587a3666145eace8715afef
k-min9 <mingu4969@gmail.com>
2024-05-19 02:04:33 +0900

    Feature: Loading component

---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="loading-container">
+      <img src="@/assets/loading.gif" alt="Loading" class="loading-image" />
+    </div>
+</template>
+  
+<script>
+  export default {
+    name: 'LoadingComponent'
+  }
+</script>
+  
+<style>
+  .loading-container {
+    width: 800px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .loading-image {
+    width: 100px;
+    height: 100px;
+  }
+</style>
+  

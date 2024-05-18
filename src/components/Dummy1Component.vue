9c057bc15a018a3e8b1b84956552f6c9b27fd681
k-min9 <mingu4969@gmail.com>
2024-05-19 02:04:23 +0900

    Feature: dummy component

---
+++ src/components/Dummy1Component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dummy1-container">
+    <img src="@/assets/dummy1.png" alt="Dummy 1" class="dummy1-image" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Dummy1Component'
+  }
+</script>
+
+<style>
+  .dummy1-image {
+    width: 800px;
+    height: 200px;
+  }
+</style>

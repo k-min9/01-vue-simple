7661f3b7b37c6f0d35525b2738d74a844fa4bf6d
k-min9 <mingu4969@gmail.com>
2024-05-22 00:46:03 +0900

    Chore: 메시지 추가

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -1,27 +1,42 @@
 <template>
-    <div class="loading-container">
-      <img src="@/assets/loading.gif" alt="Loading" class="loading-image" />
-    </div>
+  <div class="loading-container">
+    <div class="loading-message"><b>{{ customerName }}</b>님의 스마트 청약을 진행중입니다.</div>
+    <div><img src="@/assets/loading.gif" alt="Loading" class="loading-image" /></div>
+  </div>
 </template>
-  
+
 <script>
-  export default {
-    name: 'LoadingComponent'
+export default {
+  name: 'LoadingComponent',
+  props: {
+    customerName: {
+      type: String,
+      required: true
+    }
   }
+}
 </script>
-  
-<style>
-  .loading-container {
-    width: 800px;
-    height: 200px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .loading-image {
-    width: 100px;
-    height: 100px;
-  }
+
+<style scoped>
+.loading-container {
+  width: 800px;
+  height: 200px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.loading-message {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  font-size: 16px;
+  text-align: left;
+}
+
+.loading-image {
+  width: 100px;
+  height: 100px;
+}
 </style>
-  
